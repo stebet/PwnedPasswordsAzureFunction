c279076ba9a6dc9b56f1620f0d090f5d52202c35
Stefán J. Sigurðarson <stefanj@ccpgames.com>
2021-06-02 23:56:33 +0000

    Cleaning up code after moving to .NET 5.

--- Functions/BlobStorageEntry.cs
+++ Functions/BlobStorageEntry.cs
@@ -9,13 +9,24 @@ namespace Functions
     public class BlobStorageEntry
     {
         /// <summary>
+        /// Initializes a new instance of <see cref="BlobStorageEntry"/>
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="lastModified"></param>
+        public BlobStorageEntry(Stream stream, DateTimeOffset? lastModified)
+        {
+            Stream = stream;
+            LastModified = lastModified;
+        }
+
+        /// <summary>
         /// Stream representing the blob contents
         /// </summary>
-        public Stream Stream { get; set; }
+        public Stream Stream { get; }
         
         /// <summary>
         /// Pointer to the DateTimeOffset for the last time that the blob was modified
         /// </summary>
-        public DateTimeOffset? LastModified { get; set; }
+        public DateTimeOffset? LastModified { get; }
     }
 }
--- Functions/Range.cs
+++ Functions/Range.cs
@@ -1,10 +1,10 @@
 ﻿using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
+
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+
+using Microsoft.Net.Http.Headers;
 
 namespace Functions
 {
@@ -13,19 +13,17 @@ namespace Functions
     /// </summary>
     public class Range
     {
-        private readonly IConfiguration _configuration;
-        private readonly ILogger _log;
-
+        private readonly BlobStorage _blobStorage;
+        
         /// <summary>
         /// Pwned Passwords - Range handler
         /// </summary>
         /// <param name="configuration">Configuration instance</param>
-        public Range(IConfiguration configuration, ILoggerFactory logFactory)
+        public Range(BlobStorage blobStorage)
         {
-            _configuration = configuration;
-            _log = logFactory.CreateLogger("Range");
+            _blobStorage = blobStorage;
         }
-        
+
         /// <summary>
         /// Handle a request to /range/{hashPrefix}
         /// </summary>
@@ -34,42 +32,39 @@ namespace Functions
         /// <param name="log">Logger instance to emit diagnostic information to</param>
         /// <returns></returns>
         [Function("Range-GET")]
-        public Task<HttpResponseData> RunAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "range/{hashPrefix}")] HttpRequestData req,
-            string hashPrefix)
+        public async Task<HttpResponseData> RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "range/{hashPrefix}")] HttpRequestData req, string hashPrefix)
         {
-            return GetData(req, hashPrefix);
-        }
-
-        /// <summary>
-        /// Get the data for the request
-        /// </summary>
-        /// <param name="req">The request message from the client</param>
-        /// <param name="hashPrefix">The passed hash prefix</param>
-        /// <param name="log">Logger instance to emit diagnostic information to</param>
-        /// <returns>Http Response message to return to the client</returns>
-        private async Task<HttpResponseData> GetData(
-            HttpRequestData req,
-            string hashPrefix)
-        {
-            if (string.IsNullOrEmpty(hashPrefix))
-            {
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "Missing hash prefix");
-            }
-
             if (!hashPrefix.IsHexStringOfLength(5))
             {
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.BadRequest, "The hash prefix was not in a valid format");
+                return InvalidFormat(req);
             }
 
-            var storage = new BlobStorage(_configuration, _log);
-            var entry = await storage.GetByHashesByPrefix(hashPrefix.ToUpper());
-            if (entry == null)
-            {
-                return PwnedResponse.CreateResponse(req, HttpStatusCode.NotFound, "The hash prefix was not found");
-            }
-            
-            var response = PwnedResponse.CreateResponse(req, HttpStatusCode.OK, null, entry.Stream, entry.LastModified);
+            BlobStorageEntry? entry = await _blobStorage.GetByHashesByPrefix(hashPrefix.ToUpper());
+            return entry == null ? NotFound(req) : File(req, entry);
+        }
+
+        private static HttpResponseData InvalidFormat(HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.BadRequest);
+            response.Headers.Add("Content-Type", "text/plain");
+            response.WriteString("The hash prefix was not in a valid format");
+            return response;
+        }
+
+        private static HttpResponseData NotFound(HttpRequestData req)
+        {
+            var response = req.CreateResponse(HttpStatusCode.NotFound);
+            response.Headers.Add("Content-Type", "text/plain");
+            response.WriteString("The hash prefix was not found");
+            return response;
+        }
+
+        private static HttpResponseData File(HttpRequestData req, BlobStorageEntry entry)
+        {
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain");
+            response.Headers.Add(HeaderNames.LastModified, entry.LastModified?.ToString("R"));
+            response.Body = entry.Stream;
             return response;
         }
     }
